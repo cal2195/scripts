9a3cab9b49db8cbe565bc722ea2445ac2c766879
cal2195 <calmart101@gmail.com>
2017-12-13 18:18:53 +0000

    Adding borg script!

---
+++ borgbackup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export REPO=''
+export BORG_PASSPHRASE=''
+export BACKUP_LOCATIONS=''
+
+export BORG_REPO=ssh://borg@nical.ddns.net:993/mnt/borg/$REPO
+
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+info "Starting Backup..."
+
+# --exclude '/tmp' for exclusions!
+borg create --progess --stats ::{hostname}-{user}-{now:%Y-%m-%dT%H:%M:%S} $BACKUP_LOCATIONS
+
+info "Backup Complete!"
